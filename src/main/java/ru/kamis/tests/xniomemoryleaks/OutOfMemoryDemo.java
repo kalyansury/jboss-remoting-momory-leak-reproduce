3fb4b8432890a021ffc5e2bcab50650cf51ecb97
Sergey A. Lisovoy <s.lisovoy@kamis.ru>
2014-09-15 09:40:46 +0400

    [m] bigger memory leak

--- src/main/java/ru/kamis/tests/xniomemoryleaks/OutOfMemoryDemo.java
+++ src/main/java/ru/kamis/tests/xniomemoryleaks/OutOfMemoryDemo.java
@@ -6,17 +6,15 @@ import org.jboss.dmr.ModelNode;
 
 public class OutOfMemoryDemo {
 	public static void main(String[] args) throws Exception {
-		
 		ModelNode operation = Operations.createReadAttributeOperation(new ModelNode().setEmptyList(), "server-state");
-		ModelControllerClient client = ModelControllerClient.Factory.create("localhost", 9990);
-		
 		for(int i=0; i<1000000; i++) {
+			ModelControllerClient client = ModelControllerClient.Factory.create("localhost", 9990);
 			client.execute(operation);
+			client.close();
 			
 			if(i % 1000 == 0) {
 				System.out.println("Processed: " + i);
 			}
 		}
-		client.close();
 	}
 }
